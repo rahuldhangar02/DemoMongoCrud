c7e6f4f06747139d4beb2cd912e18d836ec71633
Rahul <rahul@gmail.com>
2021-04-16 17:47:19 +0530

    first Commit

---
+++ src/main/java/com/crud/CrudWithMangoDb11Application.java
@@ -0,0 +1,13 @@
+package com.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudWithMangoDb11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudWithMangoDb11Application.class, args);
+	}
+
+}
